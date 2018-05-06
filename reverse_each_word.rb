3c561da6ba1cf6f0354f6293c3125dcba7fd4278
wley3337@gmail.com <wley3337@gmail.com>
2018-05-06 01:10:35 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
-def reverse_each_word(sentence1)
-  sentence1_array = sentence1.split(" ")
-  sentence1_array.each { |x|
-  x.reverse!
-  }
-  sentence1_array.join(" ")
+# def reverse_each_word(sentence1)
+#   sentence1_array = sentence1.split(" ")
+#   sentence1_array.each { |x|
+#   x.reverse!
+#   }
+#   sentence1_array.join(" ")
   
-end
+# end
 
 def reverse_each_word(sentence1)
  sentence1_array = sentence1.split(" ")
